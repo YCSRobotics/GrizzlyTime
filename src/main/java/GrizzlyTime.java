460b983386a05e8f04cf8900f65dc9317c68ca1c
Daltz333 <gamefollower26@gmail.com>
2018-12-15 19:24:52 -0500

    A kinda... splash screen

--- src/main/java/GrizzlyTime.java
+++ src/main/java/GrizzlyTime.java
@@ -7,13 +7,6 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GrizzlyTime extends Application {
-    static{
-        CVHelper.loadLibrary();
-    }
-
-    private ImageProcess processor = new ImageProcess();
-    private GrizzlyTimeGUI userInterface = new GrizzlyTimeGUI();
-
     @Override
     public void start(Stage primaryStage) {
         primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("images/icon.png")));
@@ -30,6 +23,10 @@ public class GrizzlyTime extends Application {
 
         primaryStage.show();
 
+        CVHelper.loadLibrary();
+
+        ImageProcess processor = new ImageProcess();
+        GrizzlyTimeGUI userInterface = new GrizzlyTimeGUI();
         //process camera frames and read barcode images
         processor.displayImage(root);
         //create UI and logic

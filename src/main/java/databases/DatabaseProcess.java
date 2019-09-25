47536fda39571505847020fee2ee379a7ba9e693
Daltz333 <darasmith@wccnet.edu>
2019-09-25 16:02:04 -0400

    Removed old imports

--- src/main/java/databases/DatabaseProcess.java
+++ src/main/java/databases/DatabaseProcess.java
@@ -19,12 +19,12 @@ import helpers.CommonUtils;
 import helpers.Constants;
 import helpers.LoggingUtils;
 
-import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.*;
+import java.net.NoRouteToHostException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -79,7 +79,6 @@ public class DatabaseProcess {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        CommonUtils.application.getHostServices().showDocument("https://google.com/");
 
         return new AuthorizationCodeInstalledApp(flow, new com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver(), new AuthorizationCodeInstalledApp.Browser() {
             @Override

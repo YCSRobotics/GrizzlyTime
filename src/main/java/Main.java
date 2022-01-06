d91222709623b8b77ea90c8e0d4e5c01ad0240d0
Daltz333 <daltzsmith@gmail.com>
2022-01-06 10:53:13 -0500

    Update build system configs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,19 @@
-import com.sun.javafx.application.LauncherImpl;
+import javafx.application.Application;
+import javafx.application.Preloader;
+import javafx.stage.Stage;
 
-    /**
+/**
      * This is the true main class of GrizzlyTime, it launches GrizzlyTime
      * through some fancy black magic to get JFX working on Java 11
      */
-public class Main {
+public class Main extends Preloader {
     public static void main(String[] args){
-        LauncherImpl.launchApplication(GrizzlyTime.class, args);
+        launch(GrizzlyTime.class, args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        
     }
 }
